8f51893791870cc57b82de65a440f85b33b5441d
Michael Free <MFree@cambridgelibraries.ca>
2022-06-23 18:25:00 -0400

    rough export working as planned

--- convert-json-to-csv.py
+++ convert-json-to-csv.py
@@ -1,5 +1,4 @@
 import json
-from multiprocessing.pool import TERMINATE
 import os
 import pandas as pd
 
@@ -38,16 +37,11 @@ def write_ticketcsv(ticketdata):
                 description_content = ''
             else:
                 print(swt.keys())
-            # write to comments txt file
-            comment_log=current_directory+"/comments/"+str(count_tickets)+".txt"
-            comment_write=open(comment_log,'w', encoding='utf-8')
-            comment_write.write(str(swt["Comments"]))
-            comment_write.close()
-            #write to tickets.csv
             tickets_write = open(current_directory+"/tickets.csv",'a+',encoding='utf-8')
-            tickets_write.write("\n"+str(count_tickets)+","+str(swt["assigned_to"])+","+str(swt["created_by"])+","+swt["created_at"]+","+swt["status"]+","+str(swt["summary"]).replace(',','.').replace('\n','')+","+description_content+","+comment_log)
+            tickets_write.write("\n"+str(count_tickets)+","+str(swt["assigned_to"])+","+str(swt["created_by"])+","+swt["created_at"]+","+swt["status"]+","+str(swt["summary"]).replace(',','.').replace('\n','')+","+description_content.replace('\'','').replace('\"','')+","+format_comments(swt["Comments"]))
             tickets_write.close()
         else:
+            print("ASSIGNED TO ISSUE")
             print(swt.keys())
 
 def user_lookup(user_id):
@@ -56,37 +50,40 @@ def user_lookup(user_id):
     user_email= user_row.EMAIL.to_string(index=False)
     return user_email
 
-with open(spiceworks_json, 'r',encoding='utf-8') as spiceworks_data:
-    swd = json.load(spiceworks_data)
-    spiceworks_users = swd["users"]
-    # maybe do some threading here
-    create_usercsv()
-    for swu in spiceworks_users:
-        write_usercsv(swu)
-    spiceworks_tickets = swd["tickets"]
-    create_ticketcsv()
-    # maybe do some threading here
-    write_ticketcsv(spiceworks_tickets)
-    spiceworks_data.close()
+def format_comments(commentdata):
+    for comments in range(len(commentdata)):
+        comment_dict = commentdata[comments]
+        ticket_comments = str("UPDATED AT: "+comment_dict['updated_at']+" BY: "+user_lookup(comment_dict['created_by'])+repr("\n")+repr(comment_dict['body']).replace(',','.').replace('\'','').replace('\"',''))
+    return ticket_comments.replace('\'','')
 
-# look up assigned_to value in user_lookup write it to a list
-read_tickets = pd.read_csv(current_directory+"/tickets.csv")
-a_id = read_tickets['ASSIGNED_ID']
-assignees = []
-for assign_id in a_id:
-    assignees.append(user_lookup(int(assign_id)))
-read_tickets.drop(columns='ASSIGNED_ID', inplace=True)
-read_tickets.insert(loc=1, column="ASSIGNED_ID", value=assignees)
-c_id = read_tickets['CREATED_ID']
-creators = []
-for create_id in c_id:
-    creators.append(user_lookup(int(create_id)))
-read_tickets.drop(columns='CREATED_ID', inplace=True)
-read_tickets.insert(loc=2, column="CREATED_ID", value=creators)
-print(read_tickets)
-read_tickets.to_csv('tickets_sofar.csv', index=False)
-#print(creators)
-'''
-#if user_id in users_csv.USERID:
-        #user_row = users_csv.loc[users_csv['USERID'] == user_id]
-'''
+def assign_userids():
+    read_tickets = pd.read_csv(current_directory+"/tickets.csv")
+    a_id = read_tickets['ASSIGNED_ID']
+    assignees = []
+    for assign_id in a_id:
+        assignees.append(user_lookup(int(assign_id)))
+    read_tickets.drop(columns='ASSIGNED_ID', inplace=True)
+    read_tickets.insert(loc=1, column="ASSIGNED_ID", value=assignees)
+    c_id = read_tickets['CREATED_ID']
+    creators = []
+    for create_id in c_id:
+        creators.append(user_lookup(int(create_id)))
+    read_tickets.drop(columns='CREATED_ID', inplace=True)
+    read_tickets.insert(loc=2, column="CREATED_ID", value=creators)
+    read_tickets.to_csv(current_directory+'tickets_excelview.csv', index=False)
+
+def create_ticketdata():
+    with open(spiceworks_json, 'r',encoding='utf-8') as spiceworks_data:
+        swd = json.load(spiceworks_data)
+        spiceworks_users = swd["users"]
+        create_usercsv()
+        for swu in spiceworks_users:
+            write_usercsv(swu)
+        spiceworks_tickets = swd["tickets"]
+        create_ticketcsv()
+        write_ticketcsv(spiceworks_tickets)
+        spiceworks_data.close()
+
+create_ticketdata()
+assign_userids()
+#codecs.open(file, 'r', encoding='unicode_escape')
