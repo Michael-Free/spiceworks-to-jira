0edd36a119d85c74ab1629c0cd409a35adc682a2
Michael Free <MFree@cambridgelibraries.ca>
2022-06-28 10:28:54 -0400

    sometimes its best not knowing how sausage is made

--- ticket_cleanup.py
+++ ticket_cleanup.py
@@ -6,6 +6,11 @@ with open('tickets.csv',encoding='utf-8') as f:
     with open('output.csv', 'a+',encoding='utf-8') as g:
         for row in reader:
             g.write("\n")
-            g.write(row[1]+","+row[2]+","+row[3]+","+row[4]+","+row[5]+","+row[6]+row[7])
-    g.close()
+            g.write(row[1]+","+row[2]+","+row[3]+","+row[4]+","+row[5]+",\""+row[6]+row[7]+"\"")
+        g.close()
 f.close()
+with open('output.csv', 'r', encoding='utf-8') as fin:
+    data = fin.read().splitlines(True)
+with open('output.csv', 'w', encoding='utf-8') as fout:
+    fout.writelines("Assignee,Reporter,Due Date,Summary,Description\n")
+    fout.writelines(data[2:])
